96f394e2abaed5baa3a2c6485b734bacc234e9bb
martin <s195469@student.dtu.dk>
2021-03-15 00:07:59 +0100

    first

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> 
+  <img alt="Vue logo" src="./assets/morologo.png">
+   <HelloWorld msg="Version"/>
 </template>
 
 <script>
